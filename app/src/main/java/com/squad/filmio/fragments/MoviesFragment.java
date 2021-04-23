e349b3fef68d8051b25c782af97b8de7c67afee4
beyrakIn <beyraklnc@gmail.com>
2021-04-23 23:57:22 +0400

    Bottom Navigation

---
+++ app/src/main/java/com/squad/filmio/fragments/MoviesFragment.java
@@ -0,0 +1,22 @@
+package com.squad.filmio.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.squad.filmio.R;
+
+public class MoviesFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_movies, container, false);
+
+        return root;
+    }
+}
