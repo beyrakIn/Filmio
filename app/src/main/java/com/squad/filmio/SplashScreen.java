ff0547913003ddeb18b1ac9ed91b6e3a3e47926c
beyrakIn <beyraklnc@gmail.com>
2021-04-30 13:08:12 +0400

    App icon

--- app/src/main/java/com/squad/filmio/SplashScreen.java
+++ app/src/main/java/com/squad/filmio/SplashScreen.java
@@ -1,14 +1,26 @@
 package com.squad.filmio;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SplashScreen extends AppCompatActivity {
+    private final int TIME = 1000;
+    static int i = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+        setSplash();
+    }
+
+    private void setSplash() {
+        new Handler().postDelayed(() -> {
+        }, TIME);
     }
 }
