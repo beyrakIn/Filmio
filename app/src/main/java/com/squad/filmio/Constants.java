d6ebe7f5e6089c2faddea9b98d16e8b1a8b36909
beyrakIn <beyraklnc@gmail.com>
2021-05-14 15:42:27 +0400

    change image url

--- app/src/main/java/com/squad/filmio/Constants.java
+++ app/src/main/java/com/squad/filmio/Constants.java
@@ -5,8 +5,9 @@ import java.util.Locale;
 public class Constants {
     public static Locale locale = Locale.getDefault();
     public static final String API_KEY = "33321493d08eb4e1a46e6ce27d379eb0";
-    public static final String POSTER_SRC = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/";
-    public static final String BACK_SRC = "https://www.themoviedb.org/t/p/w533_and_h300_bestv2";
+    public static final String IMG_SRC = "https://image.tmdb.org/t/p/w500";
+    public static final String POSTER_SRC = IMG_SRC;    //"https://www.themoviedb.org/t/p/w600_and_h900_bestv2";
+    public static final String BACK_SRC = IMG_SRC;      //"https://www.themoviedb.org/t/p/w533_and_h300_bestv2";
     public static final String URL = "https://api.themoviedb.org/3/";
     public static final String LANG = locale.getLanguage();
     public static final String BANNER_ID = "ca-app-pub-2248916584991987/8089083633";
